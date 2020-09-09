c457e102b165ba36a93f374cb55bff7326bf5309
中島浩二 <knaka0209@gmail.com>
2020-09-10 08:19:30 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+﻿# react-idx
+
+ Version: 0.9.1
+
+ Author  : Kouji Nakashima / kuc-arc-f.com
+
+ date    :  2020/09/09 
+
+ update :
+
+***
+
+Github pages file , build from React
+
+***
+### sample
+
+***
+
---
+++ precache-manifest.40189ef9856500fec135eeb5fd27260e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3df3463820596488ca7ced62f065dec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "065911821b10f9230b24",
+    "url": "./static/css/main.d402f549.chunk.css"
+  },
+  {
+    "revision": "a16781ab58e987aa3570",
+    "url": "./static/js/2.4668702c.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "./static/js/2.4668702c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "065911821b10f9230b24",
+    "url": "./static/js/main.c5b022b6.chunk.js"
+  },
+  {
+    "revision": "885ab92b3307482e749f",
+    "url": "./static/js/runtime-main.24cec022.js"
+  }
+]);
